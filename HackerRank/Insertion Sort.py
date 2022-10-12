0b38a4ce25f5470c825894a71ea6822330215fa4
CarmelZe <92999610+CarmelZe@users.noreply.github.com>
2022-10-12 11:55:46 +0300

    Add files via upload

---
+++ HackerRank/Insertion Sort.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 12 11:37:04 2022
+
+@author: carme
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort1(n, arr):
+    insertedItem = arr[-1]
+    firstIndex = n-2
+    
+    while(insertedItem<arr[firstIndex]) and (firstIndex>=0):
+        arr[firstIndex+1] = arr[firstIndex]
+        print(*arr)
+        firstIndex-=1
+    arr[firstIndex+1] = insertedItem
+    print(*arr)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
