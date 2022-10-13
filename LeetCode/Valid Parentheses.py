fe0bb3166154b6b37be18cdc1cab6fbdf24d5066
CarmelZe <92999610+CarmelZe@users.noreply.github.com>
2022-10-13 16:23:36 +0300

    Add files via upload

---
+++ LeetCode/Valid Parentheses.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 13 16:15:11 2022
+
+@author: carme
+"""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        checkSigns = {'(' : ')', '[' : ']', '{' : '}'}
+        newStack = []
+        for i in s:
+            if i in checkSigns.keys():
+                newStack.append(i)
+            else:
+                if newStack == []:
+                    return False
+                a = newStack.pop()
+                if i!= checkSigns[a]:
+                    return False
+        return newStack == []
